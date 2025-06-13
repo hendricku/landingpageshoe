df44723d07e8d80aad0922d681a9029292229564
hendricku <hendrickpaul.castro@lorma.edu>
2025-06-13 11:00:57 +0800

    sthird

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { motion } from 'framer-motion';
-import Image from 'next/image';
+
 import ProductGrid from './components/ProductGrid';
 
 export default function ShoeLandingPage() {
